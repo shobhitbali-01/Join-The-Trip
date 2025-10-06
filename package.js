927c941bfdb7dd54b1aa03297fe971b016568126
shobhitbali-01 <shobhit.23gcebme060@galgotiacollege.edu>
2025-10-07 00:05:58 +0530

    Update and rename package.json to package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "JoinTheTrip",
+  description: "Group travel in India"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
