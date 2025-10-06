65462eeb8a2da85d0fc9a21a9f5dc3eba3f692a6
shobhitbali-01 <shobhit.23gcebme060@galgotiacollege.edu>
2025-10-06 23:57:50 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "JoinTheTrip",
+  description: "Group travel in India"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
